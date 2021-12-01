687c78d4e7562fdce5dffc577c881016b8f4339d
Nitin Jain <nitin.jain@windriver.com>
2021-12-01 14:42:12 +0100

    Fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,13 +2,11 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "nitinjainrg"
     storage_account_name = "njtfstatedevops"
-    container_name       = "test"
-    key                  = "test.tfstate"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
   }
 }
 provider "azurerm" {
-  # The "feature" block is required for AzureRM provider 2.x.
-  # If you're using version 1.x, the "features" block is not allowed.
   version = "~>2.0"
   features {}
 }
