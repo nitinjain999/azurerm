934b0d34907deb0be844749a512cd396d8c2d715
Nitin Jain <nitin.jain@windriver.com>
2021-12-01 14:31:08 +0100

    Adding Tf

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "nitinjainrg"
+    storage_account_name = "njtfstatedevops"
+    container_name       = "test"
+    key                  = "test.tfstate"
+  }
+}
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x.
+  # If you're using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
+  features {}
+}
+ 
+data "azurerm_client_config" "current" {}
